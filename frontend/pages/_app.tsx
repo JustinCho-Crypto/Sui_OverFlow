b982b37200957b8def7aca2b9e57d0afb746f613
Jongwoong97 <a55245063@gmail.com>
2025-05-19 12:08:55 +0000

    Favicon 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,26 +1,36 @@
 "use client";
 
-import '@mysten/dapp-kit/dist/index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
-import { getFullnodeUrl } from '@mysten/sui/client';
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import "@mysten/dapp-kit/dist/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+
 const queryClient = new QueryClient();
 const networks = {
-  devnet: { url: getFullnodeUrl('devnet') },
-  mainnet: { url: getFullnodeUrl('mainnet') },
-  testnet: { url: getFullnodeUrl('testnet') },
+  devnet: { url: getFullnodeUrl("devnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
 };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SuiClientProvider networks={networks} defaultNetwork="testnet">
-        <WalletProvider>
-          <Component {...pageProps} />
-        </WalletProvider>
-      </SuiClientProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Charui - Proof of Charity</title>
+        <meta name="description" content="Charui - Proof of Charity Platform" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <SuiClientProvider networks={networks} defaultNetwork="testnet">
+          <WalletProvider>
+            <Component {...pageProps} />
+          </WalletProvider>
+        </SuiClientProvider>
+      </QueryClientProvider>
+    </>
   );
 }
