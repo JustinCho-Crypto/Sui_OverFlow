28ad17e5c8add747c2d237411233a573b0098a80
JustinCho-Crypto <denovejr@gmail.com>
2025-05-21 22:58:04 +0900

    change package

--- bot/src/config.ts
+++ bot/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   rpcUrl: "https://fullnode.testnet.sui.io:443",
   packageId:
-    "0xa913ee311283b5ab48241b0e31714f85187484dd085bb236233c3971c3e1324e", // Module deployment address
+    "0x50ede43d1e9acfd7df19a011ff15f96f0c184358842161f5977b13630d3e02d2", // Module deployment address
   moduleName: "vault",
   functionName: "update_vault",
   objectId:
